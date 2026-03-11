fdef20a7115e18a2f5ecdf0d6c475cb2ea4faf18
MarekTalpy <marek.talpy@gmail.com>
2026-03-11 21:18:22 +0100

    Create action buttons for navbar and overlay with side menu

--- ui/src/components/layout/nav/NavBar.tsx
+++ ui/src/components/layout/nav/NavBar.tsx
@@ -3,34 +3,55 @@ import appLogo from '@/assets/images/logo-title.png';
 import './NavBar.css';
 import NavButton from './NavButton';
 import NavMenu from './NavMenu';
+import NavBarActions from './NavBarActions';
+import { useState } from 'react';
+import SideMenu from './SideMenu';
+import Overlay from '../Overlay';
 
-const NavBar = () => (
-  <div className="navbar">
-    <nav className="navbar__nav">
-      <ul className="navbar__nav-list">
-        <li className="navbar__nav-list-item">
-          <NavButton title="men">
-            <NavMenu columns={menuItems.men} />
-          </NavButton>
-        </li>
-        <li className="navbar__nav-list-item">
-          <NavButton title="women">
-            <NavMenu columns={menuItems.women} />
-          </NavButton>
-        </li>
-        <li>
-          <NavButton title="socks" />
-        </li>
-        <li>
-          <NavButton title="sale" linkStyle={{ color: 'var(--color-danger)' }} />
-        </li>
-      </ul>
-    </nav>
-    <div className="navbar__logo-container">
-      <img src={appLogo} alt="App logo" height={40} />
+const NavBar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleSearch = () => {};
+  const handleCartOpen = () => {};
+  const handleMenuOpen = () => {
+    setIsMenuOpen(true);
+  };
+
+  return (
+    <div className="navbar">
+      <nav className="navbar__nav">
+        <ul className="navbar__nav-list">
+          <li className="navbar__nav-list-item">
+            <NavButton title="men">
+              <NavMenu columns={menuItems.men} />
+            </NavButton>
+          </li>
+          <li className="navbar__nav-list-item">
+            <NavButton title="women">
+              <NavMenu columns={menuItems.women} />
+            </NavButton>
+          </li>
+          <li>
+            <NavButton title="socks" />
+          </li>
+          <li>
+            <NavButton title="sale" linkStyle={{ color: 'var(--color-danger)' }} />
+          </li>
+        </ul>
+      </nav>
+      <div className="navbar__logo-container">
+        <img src={appLogo} alt="App logo" height={40} />
+      </div>
+      <div className="navbar__actions">
+        <NavBarActions onSearch={handleSearch} onCartOpen={handleCartOpen} onMenuOpen={handleMenuOpen}></NavBarActions>
+      </div>
+
+      <Overlay isOpen={isMenuOpen} onClose={() => setIsMenuOpen(false)} />
+      <SideMenu isOpen={isMenuOpen}>
+        <dialog>Menu content</dialog>
+      </SideMenu>
     </div>
-    <div className="navbar__actions">Right side with buttons</div>
-  </div>
-);
+  );
+};
 
 export default NavBar;

685b3aa5f48210f54a6fce537d6043714bf9985b
MarekTalpy <marek.talpy@gmail.com>
2026-03-18 22:19:07 +0100

    Add new type for searched products

--- ui/src/lib/types.ts
+++ ui/src/lib/types.ts
@@ -9,3 +9,15 @@ export interface INavMenuItem {
   link: string;
   text: string;
 }
+
+export interface IPrice {
+  amount: number;
+  currency: string;
+}
+
+export interface ISearchedProduct {
+  id: string;
+  name: string;
+  imgUrl: string;
+  price: IPrice;
+}

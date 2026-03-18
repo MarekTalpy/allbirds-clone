1ac973972bbf4a7ba489908affb35d1ccc6e7fe5
MarekTalpy <marek.talpy@gmail.com>
2026-03-18 22:28:33 +0100

    Add async decoding for image

--- ui/src/components/SearchedProductsItem.tsx
+++ ui/src/components/SearchedProductsItem.tsx
@@ -16,7 +16,7 @@ const SearchedProductsItem = ({ product }: SearchedProductsItemProps) => {
   return (
     <Link to={`/products/${id}`}>
       <article className="searched-products-item__wrapper">
-        <img alt={name} src={imgUrl} width={IMG_SIZE} height={IMG_SIZE} loading="lazy"></img>
+        <img alt={name} src={imgUrl} width={IMG_SIZE} height={IMG_SIZE} loading="lazy" decoding="async"></img>
         <div>
           <h3 className="searched-products-item__name">{name}</h3>
           <p className="searched-products-item__price">{formatPrice(price.amount)}</p>

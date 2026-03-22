c4d6271f84e5dda1f1d7116de254a529bd683c4d
MarekTalpy <marek.talpy@gmail.com>
2026-03-22 22:36:03 +0100

    Create not found page

--- ui/src/components/pages/NotFound.tsx
+++ ui/src/components/pages/NotFound.tsx
@@ -1,5 +1,19 @@
+import { useNavigate } from 'react-router';
+
+import CustomButton from '../ui/CustomButton';
+import './NotFound.css';
+
 const NotFound = () => {
-  return <>NotFound</>;
+  const navigate = useNavigate();
+
+  return (
+    <section className="not-found">
+      <div className="not-found__main">
+        <h1 className="not-found__title">Page not found</h1>
+        <CustomButton label="CONTINUE SHOPPING" onClick={() => navigate('/collections/all')} />
+      </div>
+    </section>
+  );
 };
 
 export default NotFound;
